0f4e9d21d1af8d053695cf0b673bd7924efd1136
hackerudroids <tomerjava@gmail.com>
2017-03-20 19:26:12 +0200

    Gson

--- src/edu/hackeru/Main.java
+++ src/edu/hackeru/Main.java
@@ -1,13 +1,18 @@
 package edu.hackeru;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            String data = HTTPUtils.getUrl("http://www.google.co.il");
+            String data = HTTPUtils.getUrl("http://api.androidhive.info/json/movies.json");
             System.out.println(data);
+
+            Gson gson = new Gson();
+            Movie[] movies = gson.fromJson(data, Movie[].class);
         } catch (IOException e) {
             e.printStackTrace();
         }
